0a2b897ab561596eddbec327827b1282883f4ae6
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-12-03 20:21:15 +0200

    add css

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ input.addEventListener('input', debounce(onSerch, DEBOUNCE_DELAY));
 function onSerch(event) {
   const searchQuery = event.target.value.trim();
   deleteInfo();
-  console.log(searchQuery);
   if (searchQuery === '') {
     return;
   }
@@ -49,13 +48,16 @@ function renderContryCard(countries) {
   const markup = countries
     .map(country => {
       return `
+      <li>
         <img src="${country.flags.svg}" alt="Flag of ${
         country.name.official
       }" width="30" hight="20">
            <b>${country.name.official}</b></p>
               <p><b>Capital</b>: ${country.capital}</p>
               <p><b>Population</b>: ${country.population}</p>
-              <p><b>Languages</b>: ${Object.values(country.languages)} </p>`;
+              <p><b>Languages</b>: ${Object.values(
+                country.languages
+              )} </p></li>`;
     })
     .join('');
   countryInfo.innerHTML = markup;
